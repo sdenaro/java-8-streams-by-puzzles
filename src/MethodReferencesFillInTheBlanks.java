e22e1b4c793f73ee3b6ef641896283a6122b550b
sal <sdenaro@gmail.com>
2017-07-26 10:06:13 -0400

    Update MethodReferencesFillInTheBlanks.java

--- src/MethodReferencesFillInTheBlanks.java
+++ src/MethodReferencesFillInTheBlanks.java
@@ -4,9 +4,9 @@ public class MethodReferencesFillInTheBlanks {
 
 	public static void main(String[] args) {
 		
-		//Stream.of("", "b", "bc").forEach(_____________________);
+		Stream.of("", "b", "bc").forEach(System.out::println );
 		
-		//Stream.of("", "b", "bc").filter(_______).forEach(System.out::println);
+		Stream.of("", "b", "bc").filter(String::isEmpty).forEach(System.out::println);
 	
 	}
 
