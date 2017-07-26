4efa6bc618a0c4ef55a1ab3028918c04dac810e4
sal <sdenaro@gmail.com>
2017-07-26 09:52:05 -0400

    Update FunctionalInterfaceFillIntheBlanks.java

--- src/FunctionalInterfaceFillIntheBlanks.java
+++ src/FunctionalInterfaceFillIntheBlanks.java
@@ -5,15 +5,16 @@ public class FunctionalInterfaceFillIntheBlanks {
 
 	public static void main(String[] args) {
 		
-		//Predicate<String> p = ___________;
+		   
+      Predicate<String> p = s -> s.isEmpty();
 		
-		//Function<String, Integer> f = ____________;
+      Function<String, Integer> f =  (s -> s.length() ) ;  
 		
-		//Consumer<Double> c = ____________________;
+		Consumer<Double> c = (d -> System.out.println(d) );
 		
 		//Supplier<Integer> s = _______________________;
 		
 		//___________ ex = x -> "".equals(x.get(0));
-		
+ 	
 	}
 }
